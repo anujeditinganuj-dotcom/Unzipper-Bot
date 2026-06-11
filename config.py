6959cf68a570aa424be2edcd5cffbf843ae3cbac
anujeditinganuj-dotcom <anujeditinganuj@gmail.com>
2026-06-11 18:21:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ import os
 
 class Config(object):
     # Mandatory
-    APP_ID       = int(os.environ.get("APP_ID", "34446649"))
-    API_HASH     = os.environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
-    BOT_TOKEN    = os.environ.get("BOT_TOKEN", "8701067336:AAEwGUgQ1z2OcTQK2x0sMOs1bcDdSbG7Dvs")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1003951808679"))
-    BOT_OWNER    = int(os.environ.get("BOT_OWNER", "7892805795"))
+    APP_ID       = int(os.environ.get("APP_ID", "37476811"))
+    API_HASH     = os.environ.get("API_HASH", "7aa60670b871050820086c6267371ee6")
+    BOT_TOKEN    = os.environ.get("BOT_TOKEN", "8274193303:AAE0-3mLMCYvZWHoz5rGf7jvKWjUHHAIddM")
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1003824246703"))
+    BOT_OWNER    = int(os.environ.get("BOT_OWNER", "8730393744"))
     MONGODB_URL  = os.environ.get("MONGODB_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
     GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN", "dtTUYSgS85ipBgOyohzyfbZ99nhyZLcd")
 
