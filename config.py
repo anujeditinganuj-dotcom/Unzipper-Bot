b1fe0a6c3c3d84f08d7db673af7d583046b4ce04
anujeditinganuj-dotcom <anujeditinganuj@gmail.com>
2026-06-12 21:55:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     # Mandatory
     APP_ID       = int(os.environ.get("APP_ID", "37476811"))
     API_HASH     = os.environ.get("API_HASH", "7aa60670b871050820086c6267371ee6")
-    BOT_TOKEN    = os.environ.get("BOT_TOKEN", "8015464564:AAFe6QCyYpfSWPGbwih_u_XejaDLcho1KOI")
+    BOT_TOKEN    = os.environ.get("BOT_TOKEN", "8015464564:AAHw6t72IJ0ecEBl8c-mEWATuD7poLbfwNM")
     LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1003955674028"))
     BOT_OWNER = int(str(os.environ.get("BOT_OWNER", "8730393744")).strip())
     MONGODB_URL  = os.environ.get("MONGODB_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
